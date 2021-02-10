c2a1c6298814a7942fa72c383fe9b0a1d9affff5
Arnaud GARIEL <arnaud.gariel@gmail.com>
2021-02-10 20:54:33 +0100

    Avancement WEB

--- dashboard.js
+++ dashboard.js
@@ -6,9 +6,140 @@
   feather.replace()
 
   // Graphs
-  var ctx = document.getElementById('myChart')
+  var ctx1 = document.getElementById('myChart1')
+  var ctx2 = document.getElementById('myChart2')
+  var ctx3 = document.getElementById('myChart3')
+  var ctx4 = document.getElementById('myChart4')
   // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
+  var myChart2 = new Chart(ctx1, {
+    type: 'line',
+    data: {
+      labels: [
+        'Dimanche',
+        'Lundi',
+        'Mardi',
+        'Mercredi',
+        'Jeudi',
+        'Vendredi',
+        'Samedi'
+      ],
+      datasets: [{
+        data: [
+          205,
+          204,
+          205,
+          206,
+          207,
+          205,
+          203
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+
+  var myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+      labels: [
+        'Dimanche',
+        'Lundi',
+        'Mardi',
+        'Mercredi',
+        'Jeudi',
+        'Vendredi',
+        'Samedi'
+      ],
+      datasets: [{
+        data: [
+          205,
+          204,
+          205,
+          206,
+          207,
+          205,
+          203
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+
+  var myChart3 = new Chart(ctx3, {
+    type: 'line',
+    data: {
+      labels: [
+        'Dimanche',
+        'Lundi',
+        'Mardi',
+        'Mercredi',
+        'Jeudi',
+        'Vendredi',
+        'Samedi'
+      ],
+      datasets: [{
+        data: [
+          205,
+          204,
+          205,
+          206,
+          207,
+          205,
+          203
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+  var myChart4 = new Chart(ctx4, {
     type: 'line',
     data: {
       labels: [
@@ -51,3 +182,24 @@
     }
   })
 })()
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
