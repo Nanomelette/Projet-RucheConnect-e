181f9d24834c6ae63ead5d5f7cc4b48660e17cdf
Arnaud GARIEL <arnaud.gariel@gmail.com>
2021-04-12 11:25:53 +0200

    update

--- dashboard.js
+++ dashboard.js
@@ -1,192 +1,10 @@
-/* globals Chart:false, feather:false */
 
-(function () {
-  'use strict'
-
-  feather.replace()
-
-  // Graphs
-  var ctx1 = document.getElementById('myChart1')
-  var ctx2 = document.getElementById('myChart2')
-  var ctx3 = document.getElementById('myChart3')
-  var ctx4 = document.getElementById('myChart4')
-  // eslint-disable-next-line no-unused-vars
-  var myChart2 = new Chart(ctx1, {
-    type: 'line',
-    data: {
-      labels: [
-        'Dimanche',
-        'Lundi',
-        'Mardi',
-        'Mercredi',
-        'Jeudi',
-        'Vendredi',
-        'Samedi'
-      ],
-      datasets: [{
-        data: [
-          205,
-          204,
-          205,
-          206,
-          207,
-          205,
-          203
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-
-  var myChart2 = new Chart(ctx2, {
-    type: 'line',
-    data: {
-      labels: [
-        'Dimanche',
-        'Lundi',
-        'Mardi',
-        'Mercredi',
-        'Jeudi',
-        'Vendredi',
-        'Samedi'
-      ],
-      datasets: [{
-        data: [
-          205,
-          204,
-          205,
-          206,
-          207,
-          205,
-          203
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-
-  var myChart3 = new Chart(ctx3, {
-    type: 'line',
-    data: {
-      labels: [
-        '1',
-        'Lundi',
-        'Mardi',
-        'Mercredi',
-        'Jeudi',
-        'Vendredi',
-        'Samedi'
-      ],
-      datasets: [{
-        data: [
-          205,
-          204,
-          205,
-          206,
-          207,
-          205,
-          203
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-  var myChart4 = new Chart(ctx4, {
-    type: 'line',
-    data: {
-      labels: [
-        'Dimanche',
-        'Lundi',
-        'Mardi',
-        'Mercredi',
-        'Jeudi',
-        'Vendredi',
-        'Samedi'
-      ],
-      datasets: [{
-        data: [
-          205,
-          204,
-          205,
-          206,
-          207,
-          205,
-          203
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-})()
 
 function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
-
+  const button = document.querySelectorAll('button');
+  button.disabled = true;
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -202,4 +20,17 @@ function openCity(evt, cityName) {
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+
+
+function  httpGet(url_web){
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url_web, false );
+  return xmlHttp.responseText;
+}
+
+var données = httpGet("BDD.txt")
+for ( var i = 0; i < données.length; i++){
+  var data=données[i];
 }
